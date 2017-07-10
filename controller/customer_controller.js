6bb76c2d377b6e95847c6104758c17ee3a827a8e
Jarrod Henderson <jarrodhenderson@Jarrods-MacBook-Pro.local>
2017-07-10 10:44:48 +0200

    i just updated the specs file

---
+++ controller/customer_controller.js
@@ -0,0 +1,17 @@
+const customer = require('../models/customerData')
+
+module.exports = {
+
+    viewCustomer(req, res){
+        
+    },
+    createCustomer(req, res){
+
+    },
+    viewSingleCustomer(req, res){
+
+    },
+    createNewCustomer(req, res){
+
+    }
+}
