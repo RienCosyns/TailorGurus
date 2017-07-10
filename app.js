bfdcc5aea45aea548aa82760000692886d9ed3aa
Jarrod Henderson <jarrodhenderson@Jarrods-MacBook-Pro.local>
2017-07-10 14:16:08 +0200

    customer VIEW AND CREATE

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
-app.use('/',customer)
+app.use('/customer',customer)
 
 
 
--- controller/customer_controller.js
+++ controller/customer_controller.js
@@ -2,16 +2,27 @@ const customer = require('../models/customerData')
 
 module.exports = {
 
-    viewCustomer(req, res){
-        res.render('add')
+    viewCustomer(req, res) {
+        customer.find({}, (err, customer) => {
+            if (err) {
+                console.log(err)
+            } {
+                res.render('customers/allCust', {customer})
+            }
+        })
     },
-    createCustomer(req, res){
+    createCustomer(req, res) {
+        res.render('customers/add')
 
     },
-    viewSingleCustomer(req, res){
+    viewSingleCustomer(req, res) {
 
     },
-    createNewCustomer(req, res){
-
+    createNewCustomer(req, res) {
+        customer.create(req.body, (err, record)=>{
+            if(err){
+                console.log(err)
+            }res.redirect('/customer')
+        })
     }
 }
